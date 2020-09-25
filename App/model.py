04dd3bde0904b92a0c3a424944ddfe321ed91c3e
Miguel Angel Cardenas Cardenas <miguelangelcardenas@MacBook-Air-de-Miguel.local>
2020-09-25 10:24:53 -0500

    Primeros cambios del codigo.

--- App/model.py
+++ App/model.py
@@ -34,19 +34,79 @@ es decir contiene los modelos con los datos en memoria
 
 """
 
+
 # -----------------------------------------------------
 # API del TAD Catalogo de accidentes
 # -----------------------------------------------------
 
-
 # Funciones para agregar informacion al catalogo
+def inicializar_catalogo():
+    catalog={"Indice_fechas":None,"Accidentes":None}
+    catalog['Accidentes'] = lt.newList('SINGLE_LINKED', compare)
+    catalog['Indice_fechas'] = om.newMap(omaptype='BST',
+                                      comparefunction=compareDates)
+    return catalog
 
+def  añadirAccidente(catalog,Accidente):
+    lt.addLast(catalog["Accidentes"],Accidente)
+    AñadirAccidenteFecha(catalog,Accidente)
+    return catalog
+
+def AñadirAccidenteFecha(catalog,Accidente):
+    Fecha = Accidente['Start_Time']
+    Fecha_accidente = datetime.datetime.strptime(Fecha, '%Y-%m-%d %H:%M:%S')
+    entry = om.get(map, Fecha_accidente.date())
+    if entry is None:
+        datentry = newDataEntry()
+        om.put(map, Fecha_accidente.date(), datentry)
+    else:
+        datentry = me.getValue(entry)
+    Añadir_Accidente_Tipo(datentry, Accidente)
+    return map
+
+def newDataEntry():
+    
+    entry = {'Severidades': None, 'Accidentes': None}
+    entry['Severidades'] = m.newMap(numelements=11,
+                                     maptype='PROBING',
+                                     comparefunction=compareOffenses)
+    entry['Accidentes'] = lt.newList('SINGLE_LINKED', compareDates)
+    return entry
+
+def Añadir_Accidente_Tipo(datentry,Accidente):
+
+    Severidad_Accidentes=datentry["Severidad"]
+    Lista_Acci=datentry["Accidentes"]
+    lt.addLast(Lista_Acci,Accidente)
+    Seventry= m.get(Severidad_Accidentes,Accidente["Severity"])
+    if Seventry == None:
+        Entry= NuevaSeveridad(Accidente["Severity"],Accidente)
+        lt.addLast(Seventry["Accidentes"],Accidente)
+        m.put(Severidad_Accidentes,Accidente["Severity"],Entry)
+    else:
+        Entry= me.getValue(Seventry)
+        lt.addLast(Entry["Accidentes"],Accidente)
+    return datentry
+
+def NuevaSeveridad(Severidad):
+    
+    Seventry = {'Severidad': None, 'Lista_Accidentes': None}
+    Seventry['Severidad'] = Severidad
+    Seventry['Lista_Accidentes'] = lt.newList('SINGLELINKED', compareOffenses)
+    return Seventry
 
 # ==============================
 # Funciones de consulta
 # ==============================
+ def Accidente_Fecha_severidad(catalog,fecha):
+
+    Accidentes_fecha= om.get(catalog["Indice_fechas"],fecha)
+    if Accidentes_fecha =! None:
+        Entry=me.getValue()
+        
 
 
 # ==============================
 # Funciones de Comparacion
 # ==============================
+def compare_accidentes():
